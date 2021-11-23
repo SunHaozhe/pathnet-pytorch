65303ecb24e6821658be6a9acf74313ea24f4670
SunHaozhe <275940200@QQ.COM>
2021-11-23 13:38:01 +0100

    the number of layers of PathNet is no longer fixed to 3

--- pathnet.py
+++ pathnet.py
@@ -12,44 +12,45 @@ class Net(nn.Module):
     def __init__(self, args):
         super(Net, self).__init__()
         self.args = args
+        self.L = args.L # number of layers
+        self.M = args.M # number of modules in each layer
+        self.N = args.N # number of active modules
+        self.neuron_num = args.neuron_num # number of neuron in each module
         self.final_layers = []
+        if args.cifar_svhn:
+            self.input_size = 32 * 32 * 3
+        else:
+            self.input_size = 28 * 28
         self.init(None)
 
     def init(self, best_path):
         if best_path is None:
-            best_path = [[None]] * self.args.L
+            best_path = [[None]] * self.L
 
-        neuron_num = self.args.neuron_num
-        module_num = [self.args.M] * self.args.L
+        neuron_num = self.neuron_num
+        module_num = [self.M] * self.L
         #module_num = self.args.module_num
 
         """Initialize all parameters"""
         # each one stores one layer of modules, i.e. one layer of PathNet
-        self.fc1 = nn.ModuleList()
-        self.fc2 = nn.ModuleList()
-        self.fc3 = nn.ModuleList()
-
-        # loop through M modules (each module in PathNet)
-        for i in range(module_num[0]):  
-            # if this module is not in an optimal path (then its parameters can be reinitialized for transfer learning)
-            if i not in best_path[0]:  
-                """All parameters should be declared as member variable, so I think this is the simplest way to do so"""
-                """A better solution: use nn.ModuleList()"""
-                if not self.args.cifar_svhn:
-                    intput_layer = nn.Linear(28*28, neuron_num)
-                else:
-                    intput_layer = nn.Linear(32*32*3, neuron_num)
-            self.fc1.append(intput_layer)
-
-        for i in range(module_num[1]):
-            if i not in best_path[1]:
-                layer1 = nn.Linear(neuron_num, neuron_num)
-            self.fc2.append(layer1)
-
-        for i in range(module_num[2]):
-            if i not in best_path[2]:
-                layer2 = nn.Linear(neuron_num, neuron_num)
-            self.fc3.append(layer2)
+        self.layers = nn.ModuleList()
+        for l in range(self.L):
+            self.layers.append(nn.ModuleList())
+        
+        # loop through L layers of PathNet
+        for l in range(self.L):
+            # loop through M modules (each module in PathNet)
+            for i in range(module_num[l]):  
+                # if this module is not in an optimal path (then its parameters can be reinitialized for transfer learning)
+                if i not in best_path[l]:  
+                    """All parameters should be declared as member variable: use nn.ModuleList()"""
+                    if l == 0:
+                        curr_module_input_size = self.input_size
+                    else:
+                        curr_module_input_size = neuron_num
+                    # initialization or reinitialization
+                    module_ = nn.Linear(curr_module_input_size, neuron_num)
+                self.layers[l].append(module_)
 
         """final layer which is not inclued in pathnet. Independent for each task"""
         if len(self.final_layers) < 1:
@@ -60,13 +61,12 @@ class Net(nn.Module):
             self.final_layers.append(self.final_layer2)
 
         trainable_params = []
-        params_set = [self.fc1, self.fc2, self.fc3]
-        for path, params in zip(best_path, params_set):
-            for i, param in enumerate(params):
+        for path, layer_ in zip(best_path, self.layers):
+            for i, module_ in enumerate(layer_):
                 if i in path:
-                    param.requires_grad = False
+                    module_.requires_grad = False
                 else:
-                    p = {'params': param.parameters()}
+                    p = {'params': module_.parameters()}
                     trainable_params.append(p)
                     
         p = {'params': self.final_layers[-1].parameters()}
@@ -77,33 +77,14 @@ class Net(nn.Module):
             self.cuda()
 
     def forward(self, x, path, last):
-        if not self.args.cifar_svhn:
-            x = x.view(-1, 28 * 28)
-        else:
-            x = x.view(-1, 32 * 32 * 3)
-        
-        # M = self.args.M  # (chongyi zheng): comment out redundant code
-        #for i in range(self.args.L):
-        y = F.relu(self.fc1[path[0][0]](x))
-        for j in range(1, self.args.N):
-            y += F.relu(self.fc1[path[0][j]](x))
-        x = y
+        x = x.view(-1, self.input_size)
 
-        y = F.relu(self.fc2[path[1][0]](x))
-        for j in range(1, self.args.N):
-            y += F.relu(self.fc2[path[1][j]](x))
-        x = y
+        for l in range(self.L):
+            y = F.relu(self.layers[l][path[l][0]](x))
+            for j in range(1, self.N):
+                y += F.relu(self.layers[l][path[l][j]](x))
+            x = y
 
-        y = F.relu(self.fc3[path[2][0]](x))
-        for j in range(1, self.args.N):
-            y += F.relu(self.fc3[path[2][j]](x))
-        x = y
-
-        '''
-        x = F.relu(self.fc1[path[0][0]](x)) + F.relu(self.fc1[path[0][1]](x)) + F.relu(self.fc1[path[0][2]](x))
-        x = F.relu(self.fc2[path[1][0]](x)) + F.relu(self.fc2[path[1][1]](x)) + F.relu(self.fc2[path[1][2]](x))
-        x = F.relu(self.fc3[path[2][0]](x)) + F.relu(self.fc3[path[2][1]](x)) + F.relu(self.fc3[path[2][2]](x))
-        '''
         x = self.final_layers[last](x)
         return x
 
